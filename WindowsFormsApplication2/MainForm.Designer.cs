e3e8af638953403981732df881de6b8d0d46cb47
Nickolay <nickolas3331@gmail.com>
2016-05-22 23:58:30 +0500

    21

---
+++ WindowsFormsApplication2/MainForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timerGetCursorPos = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(189, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(1, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // timerGetCursorPos
+            // 
+            this.timerGetCursorPos.Interval = 2000;
+            this.timerGetCursorPos.Tick += new System.EventHandler(this.timerGetCursorPos_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(68, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timerGetCursorPos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApplication2/MainForm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class MainForm : Form
+    {
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+        Color Pixel;
+
+        int CoordX =-443;
+        int CoordY = 923;
+        int OverPointX = -279;
+        int OverPointY = 451;
+
+
+        Random rnd = new Random();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Pixel = GetColorAt(CoordX, CoordY);
+            timerGetCursorPos.Start(); //старт таймера для получения координат курсора
+        }
+
+        private void button1_Click(object sender, EventArgs e)  //Нажатие кнопки
+        {
+            for (int i = 1; i < 500; i++)
+            {
+                GOGO();                   //Запуск метода
+            }
+        }
+
+         public void GOGO()
+        {
+            //int random = rnd.Next(230, 1234);
+            Thread.Sleep(4000);                     //ждем 2000 мс
+            int OverPointX = 816;
+            int OverPointY = 647;
+            label2.Text = "3r423423";
+            Cursor.Position = new Point(OverPointX, OverPointY);
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, OverPointX, OverPointY, 0, 0);
+           Thread.Sleep(3000);
+            OverPointX = 522;
+            OverPointY = 517;
+            Cursor.Position = new Point(OverPointX, OverPointY);
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, OverPointX, OverPointY, 0, 0);
+            Thread.Sleep(3000);
+            OverPointX = 212;
+            OverPointY = 698;
+            Cursor.Position = new Point(OverPointX, OverPointY);
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, OverPointX, OverPointY, 0, 0);
+            
+            //SendKeys.Send("d");                //эмуляция нажатия ALT+F4
+        }
+        
+        private void timerGetCursorPos_Tick(object sender, EventArgs e)
+        {
+            int x = Cursor.Position.X;
+            int y = Cursor.Position.Y;
+            label1.Text =  x + "  " + y; //получаем координаты курсора
+            var c = GetColorAt(CoordX, CoordY);
+            //this.BackColor = c;
+            label2.Text = Pixel.ToString();
+           /*if (c != Pixel)
+            {
+                timerGetCursorPos.Stop();
+                Cursor.Position = new Point(OverPointX, OverPointY);  //перемещение курсора
+                for (int l = 1; l < 45;l++)
+                {
+                  //  GOGO();
+                }
+                
+
+            }*/
+        }
+        
+        //получить цвет пикселя
+        Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+        public Color GetColorAt(int x, int y)
+        {
+            using (Graphics gdest = Graphics.FromImage(screenPixel))
+            {
+                using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    IntPtr hSrcDC = gsrc.GetHdc();
+                    IntPtr hDC = gdest.GetHdc();
+                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, x, y, (int)CopyPixelOperation.SourceCopy);
+                    gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
+            
+            return screenPixel.GetPixel(0, 0);
+
+        }
+    }
+}
